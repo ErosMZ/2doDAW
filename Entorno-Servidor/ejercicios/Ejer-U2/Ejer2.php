b22344e71116e8375fdbf0615c8295edc96b3268
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2024-10-07 20:33:16 +0200

    Delete Entorno-Servidor/ejercicios directory

--- Entorno-Servidor/ejercicios/Ejer-U2/Ejer2.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* @author: Eros Muñoz Zanón
-*Modifica el ejercicio anterior para que muestre el mismo mensaje usando un operador de
-*concatenación
-*/
-   $nombre = readline("Dime tu nombre: ");
-   echo "Hola" . " $nombre\n";
-?>
