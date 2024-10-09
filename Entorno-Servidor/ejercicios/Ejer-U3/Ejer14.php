44112e768858068b91f708b445ae1bad585f05fd
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2024-10-09 22:44:54 +0200

    Update Ejer14.php

--- Entorno-Servidor/ejercicios/Ejer-U3/Ejer14.php
+++ Entorno-Servidor/ejercicios/Ejer-U3/Ejer14.php
@@ -1,9 +1,23 @@
 <?php
 
     function potencias($base, $exponente){
-
+        $operacion = "$base^$exponente: ";
+        $acumulador = 1;
+        for ($i=0; $i < $exponente; $i++) { 
+            $acumulador = $acumulador * $base; 
+            if ($i != $exponente - 1) {
+                $operacion .= "$exponente x ";
+            }else {
+                $operacion .= "$exponente";
+            }
+        }
+        echo "$operacion = $acumulador\n";
+       
     }
-
+            
     $numBase = readline("Dime la base: ");
-    $numExp = readline("Dime el exponente: ") 
+    $numExp = readline("Dime el exponente: ");
+
+    potencias($numBase, $numExp);
+
 ?>
