7c9c47ebb34211e4a94d9c01289e4cdc8c1b997d
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2024-10-14 14:42:43 +0200

    Delete Entorno-Servidor/ejercicios/Ejer-U3 directory

--- Entorno-Servidor/ejercicios/Ejer-U3/Ejer19.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $numero = readline("Dime el número: ");
-    $arrayNum = str_split($numero);
-
-    $numDigitos = count($arrayNum);
-
-    echo "El número $numero tiene " . $numDigitos . " dígitos\n"
-?>
