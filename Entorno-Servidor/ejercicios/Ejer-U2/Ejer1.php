00196bd240e44e653e1eaf64e9079725c4f64b01
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2024-10-15 19:52:21 +0200

    Delete Entorno-Servidor/ejercicios directory

--- Entorno-Servidor/ejercicios/Ejer-U2/Ejer1.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Pide un nombre, guárdalo en una variable y muestra un mensaje como “Hola Nombre,
-* encantado de conocerte”
-* @author: Eros Muñoz Zanón
-*/
-   $nombre = readline("Dime tu nombre: ");
-   echo "Hola $nombre, encantado de conocerte\n";
-?>
