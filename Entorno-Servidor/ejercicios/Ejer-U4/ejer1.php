4e3c58732053fe24e86355380eef849c7297b83a
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2024-10-17 20:23:32 +0200

    Update ejer1.php

--- Entorno-Servidor/ejercicios/Ejer-U4/ejer1.php
+++ Entorno-Servidor/ejercicios/Ejer-U4/ejer1.php
@@ -13,37 +13,37 @@
     <h1>Eros - Formulario de registro</h1>
 
     <!-- Formulario -->
-    <form action="" method="get">
+    <form action="ejer1.php" method="get">
 
     <!-- Campo de Nombre -->
     <p>
         <label for="nombre"> Nombre: </label><br>
-        <input type="text" name="nombre" maxlength="50" id="nombre" required>
+        <input type="text" name="nombre" maxlength="50" id="nombre" >
     </p>
 
     <!-- Campo de Apellidos -->
     <p>
         <label for="apellidos"> Apellidos:</label><br>
-        <input type="text" name="apellidos" maxlength="200" id="apellidos" required>
+        <input type="text" name="apellidos" maxlength="200" id="apellidos" >
     </p>
 
     <!-- Campo de Sexo -->
     <p>
         <label for="sexo"> Sexo: </label><br>
-        <input type="radio" name="sexo" id="radio-sexo-hombre" value="hombre" required> Hombre<br>
-        <input type="radio" name="sexo" id="radio-sexo-mujer" value="mujer" required> Mujer
+        <input type="radio" name="sexo" id="radio-sexo-hombre" value="hombre" > Hombre<br>
+        <input type="radio" name="sexo" id="radio-sexo-mujer" value="mujer" > Mujer
     </p>
 
     <!-- Campo de Correo electrónico -->
     <p>
         <label for="correo"> Correo electrónico: </label><br>
-        <input type="email" name="correo" id="correo" maxlength="250" required>
+        <input type="email" name="correo" id="correo" maxlength="250" >
     </p>
 
     <!-- Campo de Provincia -->
     <p>
         <label for="provincia">Provincia</label><br>
-        <select name="provincia" id="provincia" required>
+        <select name="provincia" id="provincia" >
             <option value="">Seleccione su provincia</option>
             <option value="alicante">Alicante</option>
             <option value="castellon">Castellón</option>
@@ -53,13 +53,13 @@
 
     <!-- Casilla de recibir información -->
     <p>
-        <input type="checkbox" name="recibir-informacion" value="recibir-informacion" id="recibir-informacion" checked>
+        <input type="checkbox" name="recibir-informacion" value="recibir-informacion" id="recibir-informacion" >
         <label for="recibir-informacion">Deseo recibir información sobre novedades y ofertas</label>
     </p>
 
     <!-- Casilla de aceptación de condiciones -->
     <p>
-        <input type="checkbox" name="chekeo-leido" id="chekeo-leido" required>
+        <input type="checkbox" name="chekeo-leido" id="chekeo-leido" >
         <label for="chekeo-leido">Declaro haber leído y aceptar las condiciones generales del programa y la normativa sobre protección de datos</label>
     </p>
 
