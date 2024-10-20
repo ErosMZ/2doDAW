9b2d4738dcef24dd697a80a13911b8e82214c8a8
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2024-10-21 01:30:06 +0200

    Update ejer1.php

--- Entorno-Servidor/ejercicios/Ejer-U4/ejer1.php
+++ Entorno-Servidor/ejercicios/Ejer-U4/ejer1.php
@@ -44,15 +44,15 @@
         <label for="provincia">Provincia</label><br>
         <select name="provincia" id="provincia" >
             <option value="">Seleccione su provincia</option>
-            <option value="alicante">Alicante</option>
-            <option value="castellon">Castellón</option>
-            <option value="valencia">Valencia</option>
+            <option value="Alicante">Alicante</option>
+            <option value="Castellón">Castellón</option>
+            <option value="Valencia">Valencia</option>
         </select>
     </p>
 
     <!-- Casilla de recibir información -->
     <p>
-        <input type="checkbox" name="recibir-informacion" value="recibir-informacion" id="recibir-informacion" >
+        <input type="checkbox" name="recibir-informacion" value="recibir-informacion" id="recibir-informacion" checked>
         <label for="recibir-informacion">Deseo recibir información sobre novedades y ofertas</label>
     </p>
 
@@ -68,6 +68,31 @@
     </p>
 
     </form>
+<?php
+        
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            
+            $nombre = isset($_GET["nombre"]) ? $_GET["nombre"] : "";
+            $apellidos = isset($_GET["apellidos"]) ? $_GET["apellidos"] : "";
+            $sexo = isset($_GET["sexo"]) ? $_GET["sexo"] : "";
+            $correo = isset($_GET["correo"]) ? $_GET["correo"] : "";
+            $provincia = isset($_GET["provincia"]) ? $_GET["provincia"] : "";
+            $recibirInformacion = isset($_GET["recibir-informacion"]) ? "Sí" : "No";
+            $chekeoLeido = isset($_GET["chekeo-leido"]) ? "Sí" : "No";
+
+            
+            echo "<h1>Datos del Formulario</h1>";
+            echo "<p><strong>Nombre:</strong> " . htmlspecialchars($nombre) . "</p>";
+            echo "<p><strong>Apellidos:</strong> " . htmlspecialchars($apellidos) . "</p>";
+            echo "<p><strong>Sexo:</strong> " . htmlspecialchars($sexo) . "</p>";
+            echo "<p><strong>Correo Electrónico:</strong> " . htmlspecialchars($correo) . "</p>";
+            echo "<p><strong>Provincia:</strong> " . htmlspecialchars($provincia) . "</p>";
+            echo "<p><strong>Desea recibir información:</strong> " . htmlspecialchars($recibirInformacion) . "</p>";
+            echo "<p><strong>Ha aceptado las condiciones:</strong> " . htmlspecialchars($chekeoLeido) . "</p>";
+        } else {
+            echo "<p>No se han enviado datos.</p>";
+        }
+?>
 
 </body>
 </html>
