25d55a762a3bcf674f7f731a2fd37c663ea13193
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2025-01-10 12:39:51 +0100

    Add files via upload

---
+++ Entorno-Servidor/ejercicios/Ejer-U5/formulariosYprocesamientos/resultado23.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+        <p>
+            <label for="">Nombre: </label>
+            <input type="text" name="nombre" id="nombre">
+        </p>
+
+        <p>
+            <label for="">Email: </label>
+            <input type="text" name="email" id="">
+        </p>
+
+        <p>
+            <label for="Edad"></label>
+            <input type="text" name="edad" id="">
+        </p>
+
+        <p>
+            <label for="">Nivel estidios: </label>
+            <select name="estudios" id="">
+                <option value="Primaria">Primaria</option>
+                <option value="Secundaria">Secundaria</option>
+                <option value="Bachiller">Bachiller</option>
+                <option value="Grado Medio">Grado Medio</option>
+                <option value="Universidad">Universidad</option>
+            </select>
+
+        </p>
+
+        <p>
+            <label for="">Situcación actual</label>
+            
+        </p>
+    </form>
+</body>
+</html>
