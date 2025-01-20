9c4edd69ecf72c95a3b49972e31b487122d454bf
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2025-01-20 17:48:00 +0100

    Update resultado25.php

--- Entorno-Servidor/ejercicios/Ejer-U5/formulariosYprocesamientos/resultado25.php
+++ Entorno-Servidor/ejercicios/Ejer-U5/formulariosYprocesamientos/resultado25.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio</title>
-</head>
- 
-<body>
-   olaoloal
-</body>
-</html>
+<?php
+    echo "<h2>Datos recibidos:</h2>";
+    echo "<p><li><strong>Nombre:</strong> " . htmlspecialchars($_GET["nombre"]) . "</li></p>";
+    echo "<p><li><strong>Contraseña:</strong> " . htmlspecialchars($_GET["contrasena"]) . "</li></p>";
+    echo "<p><li><strong>Nivel de estudios:</strong> " . htmlspecialchars($_GET["estudios"]) . "</li></p>";
+    echo "<p><li><strong>Nacionalidad:</strong> " . htmlspecialchars($_GET["nacionalidad"]) . "</li></p>";  
+    echo "<p><li><strong>Idiomas:</strong> " . htmlspecialchars($_GET["idiomas"]) . "</li></p>";
+    echo "<p><li><strong>Email:</strong> " . htmlspecialchars($_GET["email"]) . "</li></p>";
+?>
+
