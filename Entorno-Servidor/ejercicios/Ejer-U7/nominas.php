5bcb65e1d6ace304c7075075e2a1091fb519bafa
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2025-02-05 13:34:19 +0100

    Update nominas.php

--- Entorno-Servidor/ejercicios/Ejer-U7/nominas.php
+++ Entorno-Servidor/ejercicios/Ejer-U7/nominas.php
@@ -1,5 +1,28 @@
 <?php
 
-    include "Ejer1.php";
+    session_start();    
+
+    echo "<h1>Bienvenido " . $_SESSION["nombre"]  . "</h1>";
+    echo "<p>Página del responsable de nóminas</p>";
+
+    $sueldos = array_column($_SESSION["empleados"], "sueldo"); 
+
+    $minimo = min($sueldos); 
+    $maximo = max($sueldos); 
+
+    $empleadoMinimo = "";
+    $empleadoMaximo = "";
+    foreach ($_SESSION["empleados"] as $empleado) {
+        if ($empleado["sueldo"] == $minimo) {
+            $empleadoMinimo = $empleado;
+        }
+
+        if ($empleado["sueldo"] == $maximo) {
+            $empleadoMaximo = $empleado;
+        }
+    }
+    
+    echo "El empleado con el sueldo mínimo es: " . $empleadoMinimo["nombre"] . " con un sueldo de " . $empleadoMinimo["sueldo"] . "<br>";
+    echo "El empleado con el sueldo mínimo es: " . $empleadoMaximo["nombre"] . " con un sueldo de " . $empleadoMaximo["sueldo"];
 
 ?>
