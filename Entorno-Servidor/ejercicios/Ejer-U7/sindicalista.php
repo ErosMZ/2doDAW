7f2dc0ef9b4e84bad0256a3377299960e4fadb43
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2025-02-05 13:34:46 +0100

    Update sindicalista.php

--- Entorno-Servidor/ejercicios/Ejer-U7/sindicalista.php
+++ Entorno-Servidor/ejercicios/Ejer-U7/sindicalista.php
@@ -1,4 +1,22 @@
 <?php
-    include "Ejer1.php";
+    
+        session_start();    
+
+    echo "<h1>Bienvenido " . $_SESSION["nombre"]  . "</h1>";
+    echo "<p>Página del sndicalista</p>";
+
+    foreach ($_SESSION["empleados"] as $empleado) {
+        if ($empleado["sueldo"] == $minimo) {
+            $empleadoMinimo = $empleado;
+        }
+
+        if ($empleado["sueldo"] == $maximo) {
+            $empleadoMaximo = $empleado;
+        }
+
+        $sumaSueldos += $empleado["sueldo"];
+    }
+
+    echo "La media de el sueldo es: " . $media;
 
 ?>
