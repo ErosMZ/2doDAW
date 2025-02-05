74e34d2e0669b1d6358810558ab871fb6b532dc7
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2025-02-05 13:34:04 +0100

    Update gerente.php

--- Entorno-Servidor/ejercicios/Ejer-U7/gerente.php
+++ Entorno-Servidor/ejercicios/Ejer-U7/gerente.php
@@ -2,7 +2,7 @@
     session_start(); 
 
     echo "<h1>Bienvenido " . $_SESSION["nombre"]  . "</h1>";
-
+    echo "<p>Pagina del gerente</p>";
     $sueldos = array_column($_SESSION["empleados"], "sueldo"); 
 
     $minimo = min($sueldos); 
@@ -10,6 +10,10 @@
 
     $empleadoMinimo = "";
     $empleadoMaximo = "";
+
+    $sumaSueldos = 0;
+    
+
     foreach ($_SESSION["empleados"] as $empleado) {
         if ($empleado["sueldo"] == $minimo) {
             $empleadoMinimo = $empleado;
@@ -18,8 +22,13 @@
         if ($empleado["sueldo"] == $maximo) {
             $empleadoMaximo = $empleado;
         }
+
+        $sumaSueldos += $empleado["sueldo"];
     }
+
+    $media = $sumaSueldos / count($_SESSION["empleados"]);
     
-    echo "El empleado con el sueldo mínimo es: " . $empleadoMinimo['nombre'] . " con un sueldo de " . $empleadoMinimo["sueldo"];
-    echo "El empleado con el sueldo mínimo es: " . $empleadoMinimo['nombre'] . " con un sueldo de " . $empleadoMaximo["sueldo"];
+    echo "El empleado con el sueldo mínimo es: " . $empleadoMinimo["nombre"] . " con un sueldo de " . $empleadoMinimo["sueldo"] . "<br>";
+    echo "El empleado con el sueldo máximo es: " . $empleadoMaximo["nombre"] . " con un sueldo de " . $empleadoMaximo["sueldo"] . "<br>";
+    echo "La media de el sueldo es: " . $media;
 ?>  
